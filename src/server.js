b370e978febe296ce66a38b10c3b7e8bbcdeb564
Mete <metlandus@hotmail.com.tr>
2025-01-27 20:59:58 +0300

    Some improvements

--- src/server.js
+++ src/server.js
@@ -1,11 +1,30 @@
 import express from "express"
 import cors from "cors"
+import pino from "pino"
+import pinoHttp from "pino-http"
+import dotenv from "dotenv"
 
-function setupServer() {
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const logger = pino();
+
+app.use(pinoHttp({ logger }));
+
+app.use(cors());
+
+export function setupServer() {
     app.get("/", (req, res) => {
+        req.log.info("Hello World route accessed");
         res.send("Hello World!")
     })
+    app.use((req, res, next) => {
+        res.status(404).send({ message: "Not Found" })
+    })
     app.listen(port, () => {
-        console.log(example)
+        logger.info(`Server is running on port ${port}`)
     })
 }
+
