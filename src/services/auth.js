968a09f735394791edc7ca5092019f7c418c995c
Mete Civelek <metlandus@hotmail.com.tr>
2025-02-24 19:52:18 +0300

    Homework Finished for Real now

--- src/services/auth.js
+++ src/services/auth.js
@@ -21,7 +21,7 @@ export const registerUser = async (payload) => {
 };
 
 export const loginUser = async (payload) => {
-	const user = findUser(payload.email);
+	const user = await findUser(payload.email);
 	if (!user) {
 		throw createHttpError(404, "User not found");
 	}
